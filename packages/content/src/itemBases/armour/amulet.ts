511b637295f4c34989ff508c02c2b36beef18f95
Edgar Nightingale <edgar.nightingale@btinternet.com>
2026-07-20 21:37:48 +0100

    Add item images

--- packages/content/src/itemBases/armour/amulet.ts
+++ packages/content/src/itemBases/armour/amulet.ts
@@ -7,7 +7,16 @@ export default buildItemBase({
 	type: "armour",
 	slot: "amulet",
 	iconPool: [
-		"https://firebasestorage.googleapis.com/v0/b/monster-manual.appspot.com/o/images%2Farmours%2F-Nz9SWjFRnu58szE_T-J?alt=media&token=e81fa74d-fb48-4a21-83fb-e12ed100f319",
+		"items/armour/neck/Neck_B_01.png",
+		"items/armour/neck/Neck_B_02.png",
+		"items/armour/neck/Neck_B_03.png",
+		"items/armour/neck/Neck_B_04.png",
+		"items/armour/neck/Neck_B_05.png",
+		"items/armour/neck/Neck_B_06.png",
+		"items/armour/neck/Neck_B_07.png",
+		"items/armour/neck/Neck_B_08.png",
+		"items/armour/neck/Neck_B_09.png",
+		"items/armour/neck/Neck_B_10.png",
 	],
 	tags: [],
 });
---
+++ packages/content/src/itemBases/armour/chain.ts
@@ -0,0 +1,16 @@
+import { buildItemBase } from "../../builders/buildItemBase";
+
+export default buildItemBase({
+	id: "base_chain",
+	name: "Chain",
+	basePrice: 130,
+	type: "armour",
+	slot: "amulet",
+	iconPool: [
+		"items/armour/neck/necklace_07.png",
+		"items/armour/neck/necklace_08.png",
+		"items/armour/neck/necklace_09.png",
+		"items/armour/neck/necklace_10.png",
+	],
+	tags: [],
+});
