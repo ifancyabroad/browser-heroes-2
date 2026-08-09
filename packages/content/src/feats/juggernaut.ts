733bae4db23077a888fac85c828ce3a0f68dffad
Edgar Nightingale <edgar.nightingale@btinternet.com>
2026-08-09 09:35:50 +0100

    Refactor code structure for improved readability and maintainability

--- packages/content/src/feats/juggernaut.ts
+++ packages/content/src/feats/juggernaut.ts
@@ -5,7 +5,7 @@ export default buildFeat({
 	name: "Juggernaut",
 	description:
 		"Armour Class increases by 4 and maximum HP by 25, but attack rolls are reduced by 4.",
-	icon: "feats/Skill_StoneBody_nb.png",
+	icon: "feats/Skill_UnholyStrength_nb.png",
 	kind: "survival",
 	category: "defensive",
 	modifiers: [
