685e4134402644b4f3b433f38e6005b1d44d610a
Edgar Nightingale <edgar.nightingale@btinternet.com>
2026-08-25 20:44:43 +0100

    feat: update Berserker and Potent Casting feats to clarify damage types and modifiers

--- packages/content/src/feats/potentCasting.ts
+++ packages/content/src/feats/potentCasting.ts
@@ -3,19 +3,20 @@ import { buildFeat } from "../builders/buildFeat";
 export default buildFeat({
 	id: "potent_casting",
 	name: "Potent Casting",
-	description: "Skill save DC increases by 4, but attack rolls are reduced by 4.",
+	description: "Magical damage is multiplied by 1.5, but saving throws are reduced by 4.",
 	icon: "feats/Aura_Wizards_nb.png",
 	kind: "spellcraft",
 	category: "offensive",
 	modifiers: [
 		{
-			type: "modifyStat",
-			stat: "saveDcBonus",
-			value: 4,
+			type: "modifyDamage",
+			damageClass: "magical",
+			operation: "multiply",
+			value: 1.5,
 		},
 		{
 			type: "modifyStat",
-			stat: "attackRollBonus",
+			stat: "savingThrowBonus",
 			value: -4,
 		},
 	],
